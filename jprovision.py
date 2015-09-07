182fdd820594254ddf4bf9d33289e5c05da9f675
Yannis Mitsos <ymitsos@noc.grnet.gr>
2015-09-07 16:19:48 +0300

    * code re-factoring,  * switch added to wait for user confirmation after show\|compare

--- jprovision.py
+++ jprovision.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-''' 
-A python script that uses junos-eznc and consequently ncclient and NETCONF to push massively 
-configuration, read from a file, to a number of Juniper equipment. Results are tracked in 
+'''
+A python script that uses junos-eznc and consequently ncclient and NETCONF to push massively
+configuration, read from a file, to a number of Juniper equipment. Results are tracked in
 log file.
 
 Usage:
@@ -21,6 +21,114 @@ from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 import jnpr.junos.exception
 from termcolor import colored
+from IPy import IP
+
+def provision(host, **kwargs):
+
+    #host = kwargs['host']
+    username = kwargs['username']
+    password = kwargs['password']
+    compareconfig = kwargs['compareconfig']
+    configuration = kwargs['configuration']
+    waitconfirm = kwargs[b'waitconfirm']
+
+    print colored("-------------------------------------------------------------------------------\n",'yellow')
+    print colored("Start committing configuration to: ","cyan") + colored("%s" % host, "yellow")
+    logging.info("Start committing configuration to %s" % host)
+
+    dev = Device(host=host,
+                 username=username,
+                 password=password,
+                 port=22,
+                 timeout=5,
+                 device_params={'name':'junos'},
+                 hostkey_verify=False)
+    try:
+        logging.info("Connecting to %s" % host)
+        #logging.debug("Connecting to %s" % host)
+        dev.open()
+    except jnpr.junos.exception.ConnectAuthError as err:
+        logging.info("Wrong username or password while connecting to %s." % host)
+        print colored("Wrong username or password while connecting to %s." ,"red") % host
+        return
+    except jnpr.junos.exception.ConnectUnknownHostError as err:
+        logging.info("Wrong hostname: %s." % host)
+        print "Host: " + colored("%s" % host, "red") + " not found. Wrong FQDN?"
+        return
+    except jnpr.junos.exception.ConnectRefusedError as err:
+        logging.info("NETCONF session to %s failed." % host)
+        print "Host: " + colored('NETCONF session to %s failed', 'red') % host
+        return
+    except jnpr.junos.exception.ConnectTimeoutError as err:
+        logging.info("Time-out error. Could not open socket to: %s." % host)
+        print "Time-out error. Could not open socket to : " + colored("%s" % host, "red")
+        return
+
+    #Create an instance of Config
+    cu = Config(dev)
+    logging.debug("Acquiring lock to %s." % host)
+
+    #lock the device
+    try:
+        cu.lock()
+    except jnpr.junos.exception.LockError as err:
+        logging.info("Error: unable to lock configuration in %s." % host)
+        print colored("Error: unable to lock configuration in %s","red") % host
+        dev.close()
+        return
+
+    #parse configuration file and load commands. Handle exceptions accordingly
+    for line in configuration:
+        if line[0] != '#':
+            logging.debug("Loading command: %s in %s " % (line.rstrip('\n'), host))
+            try:
+                cu.load(line, format="set", merge=False)
+            except jnpr.junos.exception.ConfigLoadError as err:
+                logging.info("Failed loading command '%s' with severity %s in %s." %  (line.rstrip('\n'), err.errs['severity'], host))
+                print colored("Loading command failed with severity: %s", 'red') % err.errs['severity']
+                if err.errs['severity'] == 'error':
+                    cu.rollback()
+                    logging.info("Commit failed. Rolling back in %s and exiting the script" % host)
+                    logging.debug("Commit failed with %s rolling back in %s" % (err, host))
+                    print colored("Exiting, configuration rolled-back","red")
+                    cu.unlock()
+                    dev.close()
+                    sys.exit(1)
+
+
+    #print "show|compare" results to stdout if requested
+    if compareconfig != 'true':
+        print (colored("\n'show | compare' output:", 'blue'))
+        print cu.diff()
+
+    if waitconfirm == b'true':
+        if kwargs['first_host'] == b'true':
+            ack = raw_input('Proceed with commiting? [y/n]: ')
+        else:
+            ack = 'yes'
+    else:
+        ack = 'yes'
+
+    if ack == 'y' or ack == 'yes':
+        try:
+            cu.commit(comment="This is netconf jprovision script")
+            logging.info("Committing to %s succeded." % host)
+            logging.debug("Committing to %s succeded." % host)
+            print colored("Succeeded","green")
+        except jnpr.junos.exception.CommitError as err:
+            cu.rollback()
+            logging.info("Commit failed rolling back in %s" % host)
+            logging.debug("Commit failed with %s rolling back in %s" % (err, host))
+            print colored("Configuration rolled-back due to commit error","red")
+    elif ack == 'n' or ack == 'no':
+        logging.info("User aborted commiting")
+        sys.stdout.write('User aborted, rolling back and exiting.\n')
+        cu.rollback()
+        sys.exit(0)
+
+    cu.unlock()
+    dev.close()
+    logging.info("Finished.")
 
 def main():
 
@@ -36,13 +144,14 @@ def main():
 
     parser.add_argument('-u', '--username', dest='username', action='store', default=os.environ['USER'], help='username to connect to netconf server.')
     parser.add_argument('-p', '--password', dest='password', action='store', help='user\'s password.')
-    parser.add_argument('-h', '--host', dest='hostname', action='store', help='netconf target instance to connect to.')
+    parser.add_argument('-t', '--target', dest='target', action='store', help='Network device to connect to. Could be a single IP e.g. 127.0.0.1 or a network e.g. 10.0.1.0/24')
     parser.add_argument('--hosts', dest='hostsfile', action='store', help='File with hostnames to apply configuration.')
     parser.add_argument('-c', '--config', dest='configfile', action='store', required=True, help='Configuration file to read.')
     parser.add_argument('--port', dest='port', action='store', help='NETCONF server port.')
     parser.add_argument('--logfile', dest='logfile', action='store', help='File to dump logging output.')
     parser.add_argument('--log', dest='loglevel', action='store', help='Loglevel. Possible values: INFO, DEBUG')
-    parser.add_argument('--no-compareconfig', dest='compareconfig', action='store_true', help='Do not display "commit \| compare" in stdin.')
+    parser.add_argument('--no-compareconfig', dest='compareconfig', action='store_false', help='Do not display "commit \| compare" in stdin.')
+    parser.add_argument('--wait-confirm', dest='waitconfirm', action='store_false', help='Wait for user confirmation after show\|compare')
 
     if printhelp:
         parser.print_help()
@@ -50,8 +159,12 @@ def main():
 
     args = parser.parse_args()
 
-    if not(bool(args.hostname) != bool(args.hostsfile)):
-        sys.stdout.write("Either -h or --hosts flag must be defined. Not both.\n")
+    if not(bool(args.target) != bool(args.hostsfile)):
+        sys.stdout.write("Either -t or --hosts flag must be defined. Not both.\n")
+        sys.exit(1)
+
+    if not(bool(args.compareconfig) or bool(args.waitconfirm)):
+        sys.stdout.write("--wait-confirm cannot be used when --no-compareconfig is set.\n")
         sys.exit(1)
 
     loglevel = args.loglevel or 'info'
@@ -73,7 +186,13 @@ def main():
         with open(args.hostsfile , mode='r') as f:
             hosts = [line.rstrip('\n') for line in f]
     else:
-        hosts.append(args.hostname)
+        #hosts.append(args.target)
+        ip = IP(args.target)
+        for i in ip:
+            if i != ip.broadcast() and i != ip.net():
+                hosts.append(i)
+                logging.debug("Adding IP: %s to hosts list" % i)
+    #sys.exit(1)
 
     logging.info("%d hosts found" % len(hosts))
 
@@ -85,90 +204,27 @@ def main():
     if not args.password:
         args.password = getpass.getpass(args.username + '\'s Password:')
 
-    for host in hosts:
-        print colored("-------------------------------------------------------------------------------\n",'yellow')
-        print colored("Start committing configuration to: ","cyan") + colored("%s" % host, "yellow")
-        logging.info("Start committing configuration to %s" % host)
+    params = {
+            'username': args.username,
+            'password': args.password,
+            'configuration': configuration,
+            'compareconfig': args.compareconfig,
+            'waitconfirm': b'false'
+            }
 
-        dev = Device(host=host,
-                     username=args.username,
-                     password=args.password,
-                     port=22,
-                     timeout=5,
-                     device_params={'name':'junos'},
-                     hostkey_verify=False)
-        try:
-            logging.info("Connecting to %s" % host)
-            #logging.debug("Connecting to %s" % host)
-            dev.open()
-        except jnpr.junos.exception.ConnectAuthError as err:
-            logging.info("Wrong username or password while connecting to %s." % host)
-            print colored("Wrong username or password while connecting to %s." ,"red") % host
-            continue
-        except jnpr.junos.exception.ConnectUnknownHostError as err:
-            logging.info("Wrong hostname: %s." % host)
-            print "Host: " + colored("%s" % host, "red") + " not found. Wrong FQDN?"
-            continue
-        except jnpr.junos.exception.ConnectRefusedError as err:
-            logging.info("NETCONF session to %s failed." % host)
-            print "Host: " + colored('NETCONF session to %s failed', 'red') % host
-            continue
-        except jnpr.junos.exception.ConnectTimeoutError as err:
-            logging.info("Time-out error. Could not open socket to: %s." % host)
-            print "Time-out error. Could not open socket to : " + colored("%s" % host, "red")
-            continue
+    #FALSE
+    if not args.waitconfirm:
+        params['waitconfirm'] = 'true'
+        params['first_host'] = b'true'
+        provision (hosts[0], **params)
+        params['first_host'] = b'false'
+        params['waitconfirm'] = b'false'
+        for host in hosts[1:]:
+            provision (host, **params)
+    else:
+        for host in hosts:
+            provision (host, **params)
 
-        #Create an instance of Config
-        cu = Config(dev)
-        logging.debug("Acquiring lock to %s." % host)
-
-        #lock the device
-        try:
-            cu.lock()
-        except jnpr.junos.exception.LockError as err:
-            logging.info("Error: unable to lock configuration in %s." % host)
-            print colored("Error: unable to lock configuration in %s","red") % host
-            dev.close()
-            continue
-
-        #parse configuration file and load commands. Handle exceptions accordingly
-        for line in configuration:
-            if line[0] != '#':
-                logging.debug("Loading command: %s in %s " % (line.rstrip('\n'), host))
-                try:
-                    cu.load(line, format="set", merge=False)
-                except jnpr.junos.exception.ConfigLoadError as err:
-                    logging.info("Failed loading command '%s' with severity %s in %s." %  (line.rstrip('\n'), err.errs['severity'], host))
-                    print colored("Loading command failed with severity: %s", 'red') % err.errs['severity']
-                    if err.errs['severity'] == 'error':
-                        cu.rollback()
-                        logging.info("Commit failed. Rolling back in %s and exiting the script" % host)
-                        logging.debug("Commit failed with %s rolling back in %s" % (err, host))
-                        print colored("Exiting, configuration rolled-back","red")
-                        cu.unlock()
-                        dev.close()
-                        sys.exit(1)
-
-        #print "show|compare" results to stdout if requested
-        if not args.compareconfig:
-            print (colored("\n'show | compare' output:", 'blue'))
-            print cu.diff()
-
-        try:
-            cu.commit(comment="This is netconf jprovision script")
-            logging.info("Committing to %s succeded." % host)
-            logging.debug("Committing to %s succeded." % host)
-            print colored("Succeeded","green")
-        except jnpr.junos.exception.CommitError as err:
-            cu.rollback()
-            logging.info("Commit failed rolling back in %s" % host)
-            logging.debug("Commit failed with %s rolling back in %s" % (err, host))
-            print colored("Configuration rolled-back due to commit error","red")
-
-        cu.unlock()
-        dev.close()
-
-    logging.info("Finished.")
     sys.exit(0)
 
 logging.getLogger('paramiko').addHandler(logging.NullHandler())
